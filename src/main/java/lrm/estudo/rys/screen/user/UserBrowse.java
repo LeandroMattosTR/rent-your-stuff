55599af1714c74f40ce4f75ec8d00a6162b22c42
LeandroMattosTR <leandro.mattos@thomsonreuters.com>
2022-05-03 18:09:08 -0300

    Initial commit

---
+++ src/main/java/lrm/estudo/rys/screen/user/UserBrowse.java
@@ -0,0 +1,12 @@
+package lrm.estudo.rys.screen.user;
+
+import lrm.estudo.rys.entity.User;
+import io.jmix.ui.navigation.Route;
+import io.jmix.ui.screen.*;
+
+@UiController("rys_User.browse")
+@UiDescriptor("user-browse.xml")
+@LookupComponent("usersTable")
+@Route("users")
+public class UserBrowse extends StandardLookup<User> {
+}
---
+++ src/test/java/lrm/estudo/rys/RentYourStuffApplicationTests.java
@@ -0,0 +1,13 @@
+package lrm.estudo.rys;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RentYourStuffApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
